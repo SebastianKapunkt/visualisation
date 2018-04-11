d19c957fc2941aec6327dfaee701a2f209982966
SebastianKapunkt <Kindt.Sebastian@outlook.com>
2018-04-11 17:01:36 +0200

    add slider to change circle size

--- visualisation01/javascript/canvas_controller.js
+++ visualisation01/javascript/canvas_controller.js
@@ -23,3 +23,35 @@ function clear_canvas(canvas) {
   var context = canvas.getContext("2d");
   context.clearRect(0, 0, canvas.width, canvas.height);
 }
+
+function slide_handling(callback) {
+  $(document).on(
+    "input change",
+    "#number_slider",
+    debounce(function() {
+      var current_slider_value = $(this).val();
+      callback(current_slider_value);
+    }, 0)
+  );
+}
+
+// source: https://davidwalsh.name/javascript-debounce-function
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+function debounce(func, wait, immediate) {
+  var timeout;
+  return function() {
+    var context = this,
+      args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
