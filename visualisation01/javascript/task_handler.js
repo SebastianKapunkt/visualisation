7afaa33abb2ba7fdf6e9b99c8d99319cf361f68f
SebastianKapunkt <Kindt.Sebastian@outlook.com>
2018-04-18 16:46:25 +0200

    join result on tasks and download as json

--- visualisation01/javascript/task_handler.js
+++ visualisation01/javascript/task_handler.js
@@ -20,7 +20,7 @@ function get_next_task() {
   }
 }
 
-function finish_and_skip(){
+function finish_and_skip() {
   current_tasks = tasks.length;
   run_calculation();
 }
@@ -29,11 +29,12 @@ function run_calculation() {
   $(".end_screen").css("display", "flex");
 
   // join the results back on the task
-  var results = tasks.map(function(item){
-    item.user_value = result_map.get(item.id)
+  var results = tasks.map(function(item) {
+    item.user_value = result_map.get(item.id);
     return item;
-  })
+  });
 
+  get_result_as_json(results);
   console.log(results);
 }
 
@@ -41,4 +42,14 @@ function save_result() {
   result_map.set(task_id, $("#number_slider").val());
 
   run_next_or_visualize();
+}
+
+function get_result_as_json(results) {
+  var dataStr =
+    "data:text/json;charset=utf-8," +
+    encodeURIComponent(JSON.stringify(results));
+  var dlAnchorElem = document.getElementById("downloadAnchorElem");
+  dlAnchorElem.setAttribute("href", dataStr);
+  dlAnchorElem.setAttribute("download", "task_results.json");
+  dlAnchorElem.click();
 }
