7b85e8d2808439d9c3df26228bf14bb0790e8448
SebastianKapunkt <Kindt.Sebastian@outlook.com>
2018-05-30 15:43:20 +0200

    Convert car.txt to javascript object

--- visualisation03/javascript/car-converter.js
+++ visualisation03/javascript/car-converter.js
@@ -1,5 +1,20 @@
 let lines = car_data.split("\n");
+let cars = [];
 lines.map( l => {
     items = l.split("	");
-    items.map( i => console.log(i));
+    var object = {
+        "Car": items[0],
+        "Manufacturer": items[1],
+        "MPG": items[2],
+        "Cylinders": items[3],
+        "Displacement": items[4],
+        "Horsepower": items[5],
+        "Weight": items[6],
+        "Acceleration": items[7],
+        "Model Year": items[8],
+        "Origin": items[9]
+    };
+    cars.push(object);
 });
+
+console.log(cars);
